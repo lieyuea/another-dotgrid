0b231510c857a31caf4f69c6499e1307cc65ea43
lieyuea <mjoshuahua@gmail.com>
2023-03-10 13:54:09 -0800

    implement finding current location in grid

--- app/main.rb
+++ app/main.rb
@@ -5,7 +5,6 @@ def init
 
   $default_keybinds = {
     fullscreen: 'p',
-    # save_state: 'o',
   }
   $keybinds ||= $default_keybinds
 
@@ -48,20 +47,49 @@ def init
     $point_cols << point_arr.last.x
   end
 
-  $outputs.static_primitives << [ $scroll_bar, scroll_bar_border, $points ]
+  $outputs.static_primitives << [ $scroll_bar, scroll_bar_border, point_arr ]
+end
+
+def find_hovered_point mouse
+  # iterating rows/cols separately avoids having to iterate 1800 times
+  # each frame when hovering over bottom right point
+  # since there are less rows than cols, check there first
+  in_row = nil
+  h = nil
+  $point_rows.each_with_index do |coord, row|
+    if mouse.inside_rect? [ 0, coord - 10, 1280, 20 ]
+      in_row = row
+      h = coord
+      break
+    end
+  end
+
+  return unless in_row
+
+  in_col = nil
+  $point_cols.each_with_index do |coord, col|
+    if mouse.inside_rect? [ coord - 10, h - 10, 20, 20 ]
+      in_col = col
+      break
+    end
+  end
+
+  return unless in_col
+
+  $points[in_col][in_row]
 end
 
 def mouse_input
   mouse = $inputs.mouse
 
-  if mouse.wheel
-    $scroll_bar.y += mouse.wheel.y * $scroll_bar.h
-    if $scroll_bar.y >= $scroll_bar.max_y
-      $scroll_bar.y = $scroll_bar.max_y
-    elsif $scroll_bar.y <= $scroll_bar.min_y
-      $scroll_bar.y = $scroll_bar.min_y
-    end
+  hovered_point = find_hovered_point mouse
+
+  if hovered_point
+    $hover_border = { x: hovered_point.x - 10, y: hovered_point.y - 10, w: 20, h: 20, primitive_marker: :border }
+  else
+    $hover_border = nil
   end
+
   # since the scroll bar is so small, just keep track of if it's held or not
   # instead of dragging inside the bar
   if mouse.click
@@ -80,12 +108,21 @@ def mouse_input
   else
     $scroll_bar_held = nil
   end
+
+  ########### TODO: scrolling mouse wheel for zoom
+  if mouse.wheel
+    $scroll_bar.y += mouse.wheel.y * $scroll_bar.h
+    if $scroll_bar.y >= $scroll_bar.max_y
+      $scroll_bar.y = $scroll_bar.max_y
+    elsif $scroll_bar.y <= $scroll_bar.min_y
+      $scroll_bar.y = $scroll_bar.min_y
+    end
+  end
 end
 
 def keyboard_input
   case $inputs.keyboard.key_down.char
     when $keybinds[:fullscreen] then $gtk.set_window_fullscreen !$gtk.instance_variable_get(:@window_is_fullscreen)
-    # when $keybinds[:save_state] then $gtk.save_state
   end
 end
 
@@ -94,14 +131,18 @@ def calc
   keyboard_input
 end
 
-def tick args
-  init
-  calc
-
+def draw
+  $outputs.primitives << $hover_border
   $outputs.debug << [
     $gtk.framerate_diagnostics_primitives,
     # $layout.debug_primitives
   ]
+end
+
+def tick args
+  init
+  calc
+  draw
 end
 
 $gtk.reset
